7fc077d0a483b0e9d0b84ca58e43d189f5495b3c
nahwoah <119922465+nahwoah@users.noreply.github.com>
2026-05-11 07:11:37 +0300

    added place marker function

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-let board = [
+const board = [
   ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ];
 
-console.log(board)
+position = 0;
 
-console.log('Select position to ')
+console.log('Select position to mark');
+
+function placeMarker(position) {
+  index = position - 1;
+  row = Math.floor(index / 3)
+  column = index % 3
+  board[row][column] = 'O';
+}
+
+// index = position - 1;
+// row = Math.floor(index / 3)
+// column = index % 3
+
+postition = Number(prompt("Enter your age:"));
+placeMarker(position);
+position = 3;
+placeMarker(position);
+position = 9;
+placeMarker(position);
+
+
+
+
+console.log(board)
