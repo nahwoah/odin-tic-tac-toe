0ac574bee36c15901a9ca6288d04ae9c8dbed367
nahwoah <119922465+nahwoah@users.noreply.github.com>
2026-05-19 15:07:49 +0300

    added position check

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ position = 0;
 
 console.log('Select position to mark');
 
+function findPosition(position){
+  index = position - 1;
+  row = Math.floor(index / 3)
+  column = index % 3
+  return board[row][column];
+}
+
 function placeMarker(position) {
   index = position - 1;
   row = Math.floor(index / 3)
@@ -15,18 +22,28 @@ function placeMarker(position) {
   board[row][column] = 'O';
 }
 
+function checkPosition(position) {
+  index = position - 1;
+  row = Math.floor(index / 3)
+  column = index % 3
+
+  if (board[row][column] !== ''){
+    return -1;
+  } else{
+    return 0;
+  }
+}
+
 // index = position - 1;
 // row = Math.floor(index / 3)
 // column = index % 3
 
-postition = Number(prompt("Enter your age:"));
-placeMarker(position);
-position = 3;
-placeMarker(position);
-position = 9;
-placeMarker(position);
-
-
+position = Number(prompt("Select position 1-9:"));
+if (!checkPosition() || position < 1 || position > 9){
+  console.log('Please choose another position');
+}else{
+  placeMarker(position);
+}
 
 
 console.log(board)
