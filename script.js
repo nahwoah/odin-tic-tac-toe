dc57b71bc54a64c34dbf091cadda1e32255f8cae
nahwoah <119922465+nahwoah@users.noreply.github.com>
2026-05-24 17:54:13 +0300

    adding checkBoard function

--- script.js
+++ script.js
@@ -34,13 +34,19 @@ function checkPosition(position) {
   }
 }
 
+function checkWinner(){
+  if (board[0][0] === board[0][1] && board[0][1] === board[0][2] && board[0][0] === 'O'){
+    
+  }
+}
+
 // index = position - 1;
 // row = Math.floor(index / 3)
 // column = index % 3
 
 while (true) {
   position = Number(prompt("Select position 1-9:"));
-  if (!checkPosition(position) || position < 1 || position > 9) {
+  if (position < 1 || position > 9 || !checkPosition(position) ) {
     console.log('Please choose another position');
   } else {
     placeMarker(position);
